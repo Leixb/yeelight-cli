5cf352f352c8840f0f307451aaa0a24b16b3bcb3
Leixb <abone9999@gmail.com>
2020-06-18 18:11:12 +0200

    better bg implementation

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,14 @@ use yeelight;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
+#[structopt(name = "yeelight-cli", about = "An CLI to control your Yeelight smart lights.")]
 struct Options {
-    #[structopt(env)]
+    #[structopt(env = "YEELIGHT_ADDR")]
     address: String,
-    #[structopt(short, long, default_value = "55443")]
+    #[structopt(short, long, default_value = "55443", env = "YEELIGHT_PORT")]
     port: u16,
     #[structopt(subcommand)]
     subcommand: Command,
-    #[structopt(long)]
-    bg: bool,
 }
 
 #[derive(Debug, StructOpt)]
@@ -19,7 +18,13 @@ enum Command {
     #[structopt(about = "Get properties")]
     Get { properties: Vec<yeelight::Property> },
     #[structopt(about = "Toggle light")]
-    Toggle,
+    #[structopt(group = structopt::clap::ArgGroup::with_name("light"))]
+    Toggle{
+        #[structopt(long, group = "light")]
+        dev: bool,
+        #[structopt(long, group = "light")]
+        bg: bool,
+    },
     #[structopt(about = "Turn on light")]
     On {
         #[structopt(short, long, default_value = "Smooth")]
@@ -28,6 +33,8 @@ enum Command {
         duration: u64,
         #[structopt(short, long, default_value = "Normal")]
         mode: yeelight::Mode,
+        #[structopt(long)]
+        bg: bool,
     },
     #[structopt(about = "Turn off light")]
     Off {
@@ -37,6 +44,8 @@ enum Command {
         duration: u64,
         #[structopt(short, long, default_value = "Normal")]
         mode: yeelight::Mode,
+        #[structopt(long)]
+        bg: bool,
     },
     #[structopt(about = "Start timer")]
     Timer { minutes: u64 },
@@ -60,20 +69,30 @@ enum Command {
         count: u8,
         #[structopt(default_value = "Recover")]
         action: yeelight::CfAction,
+        #[structopt(long)]
+        bg: bool,
     },
     #[structopt(about = "Stop color flow")]
-    FlowStop,
+    FlowStop{
+        #[structopt(long)]
+        bg: bool,
+    },
     #[structopt(about = "Adjust properties (Bright/CT/Color) (increase/decrease/circle)")]
     Adjust {
         property: yeelight::Prop,
         action: yeelight::AdjustAction,
+        #[structopt(long)]
+        bg: bool,
     },
     #[structopt(about = "Adjust properties (Bright/CT/Color) with perentage (-100~100)")]
+    #[structopt(setting = structopt::clap::AppSettings::AllowNegativeNumbers)]
     AdjustPercent {
         property: yeelight::Prop,
         percent: i8,
         #[structopt(default_value = "500")]
         duration: u64,
+        #[structopt(long)]
+        bg: bool,
     },
     #[structopt(about = "Connect to music TCP stream")]
     MusicConnect { host: String, port: u32 },
@@ -87,20 +106,30 @@ enum Prop {
         power: yeelight::Power,
         #[structopt(default_value = "Normal")]
         mode: yeelight::Mode,
+        #[structopt(long)]
+        bg: bool,
     },
     CT {
         color_temperature: u64,
+        #[structopt(long)]
+        bg: bool,
     },
     RGB {
         rgb_value: u32,
+        #[structopt(long)]
+        bg: bool,
     },
     HSV {
         hue: u16,
         #[structopt(default_value = "100")]
         sat: u8,
+        #[structopt(long)]
+        bg: bool,
     },
     Bright {
         brightness: u8,
+        #[structopt(long)]
+        bg: bool,
     },
     Name {
         name: String,
@@ -112,8 +141,22 @@ enum Prop {
         val2: u64,
         #[structopt(default_value = "100")]
         val3: u64,
+        #[structopt(long)]
+        bg: bool,
     },
-    Default,
+    Default {
+        #[structopt(long)]
+        bg: bool,
+    },
+}
+
+macro_rules! sel_bg {
+    ($obj:tt.$fn:ident ($($p:expr),*) || $fn_bg:ident if $var:tt ) => (
+        match $var {
+            true  => $obj.$fn_bg($($p),*),
+            false => $obj.$fn($($p),*),
+        }
+    );
 }
 
 fn main() {
@@ -121,41 +164,46 @@ fn main() {
 
     let mut bulb = yeelight::Bulb::connect(&opt.address, opt.port).unwrap();
 
-    let bg = opt.bg;
-
     let response = match opt.subcommand {
-        Command::Toggle => bulb.ch_toggle(bg),
+        Command::Toggle{bg, dev} => {
+            match (bg, dev) {
+                (true, _) => bulb.bg_toggle(),
+                (_, true) => bulb.dev_toggle(),
+                _ => bulb.toggle(),
+            }
+        },
         Command::On {
             effect,
             duration,
             mode,
-        } => bulb.ch_set_power(bg, yeelight::Power::On, effect, duration, mode),
+            bg,
+        } => sel_bg!(bulb.set_power(yeelight::Power::On, effect, duration, mode) || bg_set_power if bg),
         Command::Off {
             effect,
             duration,
             mode,
-        } => bulb.ch_set_power(bg, yeelight::Power::Off, effect, duration, mode),
+            bg,
+        } => sel_bg!(bulb.set_power(yeelight::Power::Off, effect, duration, mode) || bg_set_power if bg),
         Command::Get { properties } => bulb.get_prop(&yeelight::Properties(properties)),
         Command::Set {
             property,
             effect,
             duration,
         } => match property {
-            Prop::Power { power, mode } => bulb.ch_set_power(bg, power, effect, duration, mode),
-            Prop::CT { color_temperature } => {
-                bulb.ch_set_ct_abx(bg, color_temperature, effect, duration)
-            }
-            Prop::RGB { rgb_value } => bulb.ch_set_rgb(bg, rgb_value, effect, duration),
-            Prop::HSV { hue, sat } => bulb.ch_set_hsv(bg, hue, sat, effect, duration),
-            Prop::Bright { brightness } => bulb.ch_set_bright(bg, brightness, effect, duration),
-            Prop::Name { name } => bulb.set_name(&name),
+            Prop::Power { power, mode, bg} => sel_bg!(bulb.set_power(power, effect, duration, mode) || bg_set_power if bg),
+            Prop::CT { color_temperature, bg} => sel_bg!(bulb.set_ct_abx(color_temperature, effect, duration) || bg_set_ct_abx if bg),
+            Prop::RGB { rgb_value, bg} => sel_bg!(bulb.set_rgb(rgb_value, effect, duration) || bg_set_rgb if bg),
+            Prop::HSV { hue, sat, bg} => sel_bg!(bulb.set_hsv(hue, sat, effect, duration) || bg_set_hsv if bg),
+            Prop::Bright { brightness, bg} => sel_bg!(bulb.set_bright(brightness, effect, duration) || bg_set_bright if bg),
+            Prop::Name { name } => bulb.set_name(yeelight::QuotedString(name)),
             Prop::Scene {
                 class,
                 val1,
                 val2,
                 val3,
-            } => bulb.set_scene(class, val1, val2, val3),
-            Prop::Default => bulb.ch_set_default(bg),
+                bg,
+            } => sel_bg!(bulb.set_scene(class, val1, val2, val3) || bg_set_scene if bg),
+            Prop::Default{bg} => sel_bg!(bulb.set_default() || bg_set_default if bg),
         },
         Command::Timer { minutes } => bulb.cron_add(yeelight::CronType::Off, minutes),
         Command::TimerClear => bulb.cron_del(yeelight::CronType::Off),
@@ -164,22 +212,24 @@ fn main() {
             count,
             action,
             expression,
-        } => bulb.ch_start_cf(bg, count, action, expression),
-        Command::FlowStop => bulb.ch_stop_cf(bg),
-        Command::Adjust { action, property } => bulb.ch_set_adjust(bg, action, property),
+            bg,
+        } => sel_bg!(bulb.start_cf(count, action, expression) || bg_start_cf if bg),
+        Command::FlowStop { bg } => sel_bg!(bulb.stop_cf() || bg_stop_cf if bg),
+        Command::Adjust { action, property, bg } => sel_bg!(bulb.set_adjust(action, property) || bg_set_adjust if bg),
         Command::AdjustPercent {
             property,
             percent,
             duration,
+            bg,
         } => match property {
-            yeelight::Prop::Bright => bulb.ch_adjust_bright(bg, percent, duration),
-            yeelight::Prop::Color => bulb.ch_adjust_color(bg, percent, duration),
-            yeelight::Prop::CT => bulb.ch_adjust_ct(bg, percent, duration),
+            yeelight::Prop::Bright => sel_bg!(bulb.adjust_bright(percent, duration) || adjust_bright if bg),
+            yeelight::Prop::Color => sel_bg!(bulb.adjust_color(percent, duration) || adjust_color if bg),
+            yeelight::Prop::CT => sel_bg!(bulb.adjust_ct(percent, duration) || adjust_ct if bg),
         },
         Command::MusicConnect { host, port } => {
-            bulb.set_music(yeelight::MusicAction::On, &host, port)
+            bulb.set_music(yeelight::MusicAction::On, yeelight::QuotedString(host), port)
         }
-        Command::MusicStop => bulb.set_music(yeelight::MusicAction::Off, "", 0),
+        Command::MusicStop => bulb.set_music(yeelight::MusicAction::Off, yeelight::QuotedString("".to_string()), 0),
     }
     .unwrap();
 
